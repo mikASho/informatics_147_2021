d6d077976eb3701e0976c3e135315d48d4283d2d
mikASho <platon11.90@mail.ru>
2021-12-02 16:16:33 +0300

    Задание 5

---
+++ Task_5_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int grtest_com_div(int a, int b);
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int a, b;
+    cout << " Введите целые положительные числа a и b: ";
+    cin >> a >> b;
+    cout << "НОД(" << a << "; " << b << ") = " << grtest_com_div(a, b);
+    return 0;
+}
+//Функция НОД
+int grtest_com_div(int a, int b){
+  if (a % b == 0){ 
+    return b;
+  }
+  if (b % a == 0){
+    return a;
+  }
+  //Рекурсия по поиску наибольшего делителя
+  if (a > b){
+    return grtest_com_div(a % b, b);
+  }
+  
+  return grtest_com_div(a, b % a);
+}
