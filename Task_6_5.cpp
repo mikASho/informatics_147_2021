09b7129337b97091f588e3e7c8a89ce9cb2f3208
mikASho <platon11.90@mail.ru>
2021-12-25 10:58:24 +0300

    Задание 6 упр 5

--- Task_6_5.cpp
+++ Task_6_5.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void simple_num(int);
+void simple_num(int); //функция нахождения простых чисел
 
 int main()
 {
@@ -10,38 +10,54 @@ int main()
     
     cout << "Введите заключающее число диапозона: ";
     cin >> n;
-    
-    
-    
+   
     simple_num(n);
     
     return 0;
 }
 
 void simple_num(int n){
-    
-    const int N = n+1;
-    int arr_smpl_nmbrs[N]{};
-    
-    for(int i = 1; i <= N; i++){
-        arr_smpl_nmbrs[i] = i;
-        cout << arr_smpl_nmbrs[i] << endl;
+
+    int arr_numbrs[]{};
+    //создаем диапозон от 2 до n
+    for(int i = 1; n >= i; i++){
+        arr_numbrs[i] = i;
     }
     
-    //int new_arr_smpl_nmbrs[N]{};
-    
-    for(int i = 1; i <= N; i++){
-        
-        for(int j = i-1; j <= N; j++){
-            if(arr_smpl_nmbrs[j] == 0){
-                continue;
+    //терзаем массив 
+    for(int i = 2; n >= i; i++){
+        for(int j = 1; j <= n; j++){
+            //если элемент массива делится на число i, то приравниваем число к i
+            if(arr_numbrs[j] % i == 0){
+                arr_numbrs[j] = i;
             }
-                else if(arr_smpl_nmbrs[j] % arr_smpl_nmbrs[i] == 0){
-                    arr_smpl_nmbrs[j] = 0;
-                }
         }
     }
-    for(int i = 0; i < N; i++){
-        cout << arr_smpl_nmbrs[i] << endl;
+    // убираем дубли в массиве
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = i + 1; j <= n ; j++)
+        {
+            if ( arr_numbrs[i] == arr_numbrs[j] ) // ищем дубль
+            {
+                for (int k_shift = j; k_shift <= n -1; k_shift++)
+                {
+                    // выполняем сдвиг элементов массива на -1, начиная со следующего элемента, после найденного дубля
+                    arr_numbrs[k_shift] = arr_numbrs[k_shift + 1];
+                }
+                n -= 1; // уменьшаем массив на 1
+ 
+                if (arr_numbrs[i] == arr_numbrs[j]) // если следующий элемент - дубль
+                {
+                 j--; // выполнияем переход на предыдущий элемент     
+                }
+            }
+        }
+    }
+    // вывод простых чисел
+    cout << "2" << endl;
+    for(int i = 3; n >= i; i++){
+            cout << arr_numbrs[i] << endl;
     }
 }
+   
